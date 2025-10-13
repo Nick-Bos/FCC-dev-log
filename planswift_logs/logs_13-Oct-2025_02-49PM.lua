5a5399d9137325fde4232ae1dfc1273346559962
Nick Bos <roofingbos@gmail.com>
2025-10-13 15:00:17 +1030

    Add initial log for part codes in 1.2 sheet width parts

---
+++ planswift_logs/logs_13-Oct-2025_02-49PM.lua
@@ -0,0 +1,8 @@
+Today I added part codes as a property to the 1.2 sheet width parts that populate from the area template.
+The logic resides in the formula feild of the "Part Code" property of the "1.2 sized" part.
+
+Status of the code as of 13-Oct-2025_02-49PM: function has only some data entered in, will need to be completed and verified with kirk at later date
+
+GitHub commit reference: planswift_formulas_functions/1.2-sheet-part-code-function.lua
+
+   
