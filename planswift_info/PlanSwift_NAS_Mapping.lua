08016e893ac40d9485d1cf3592718e9e86973fbb
Nick Bos <roofingbos@gmail.com>
2025-10-14 13:20:02 +1030

    Add initial PlanSwift NAS mapping instructions

---
+++ planswift_info/PlanSwift_NAS_Mapping.lua
@@ -0,0 +1,4 @@
+1. Instuctions for pointing planswift to the NAS drive for global lists
+2. Ensure Planswift is closed and other planswift users are not editing lists globally befor next step
+3. Run "LinkListsOnly.bat" file as admimistrator, can be found in this repository.
+4. Open Planswift and check lists are working.
