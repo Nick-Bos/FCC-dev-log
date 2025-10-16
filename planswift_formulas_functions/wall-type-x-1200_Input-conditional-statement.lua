ad470d41b4b9cd3d3773ebef827363eae7dc39d2
Nicholas Bos <90960995+Nick-Bos@users.noreply.github.com>
2025-10-16 15:40:05 +1030

    Refine conditional statement
    This resolves issue where ceiling height = 3.0 was not being read in the input fuction logic
    Added a value range to solve the floating integer issue

--- planswift_formulas_functions/wall-type-x-1200_Input-conditional-statement.lua
+++ planswift_formulas_functions/wall-type-x-1200_Input-conditional-statement.lua
@@ -1,4 +1,5 @@
 // Condition for wall type '10RE' based on ceiling heights
 // This condition checks if the wall type is '10RE' and if the ceiling height is one of the specified values 
 // (2.4, 3.0, 3.6, or 4.2 meters). If both conditions are met, it returns true; otherwise, it returns false.
-'[Wall Type]' = '10RE' and ('[Ceiling Height]' = 2.4 or '[Ceiling Height]' = 3.0 or '[Ceiling Height]' = 3.6 or '[Ceiling Height]' = 4.2)
+('[Wall Type]' = '10RE') and (('[Ceiling Height]' = 2.4) or ('[Ceiling Height]' = 3.6) or (([Ceiling Height] > 2.9) and ([Ceiling Height] < 3.1)) or ('[Ceiling Height]' = 4.2))
+      
