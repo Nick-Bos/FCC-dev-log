ece8d8babc97b14c2f3342cbd40d199ca4dcae9e
Nick Bos <roofingbos@gmail.com>
2025-10-16 17:46:54 +1030

    Add log file for 16-Oct-2025 to document sheet length bug fix

---
+++ planswift_logs/logs_16-Oct-2025_05-31PM.lua
@@ -0,0 +1,7 @@
+Fixed bug with sheet length feild not populating when ceiling height is 3.0
+refer to commit: 'planswift_formulas_functions/wall-type-x-1200_Input-conditional-statement.lua'
+for more details.
+
+Items for friday:
+- speak with kirk about over estimation formula for ceiling sheet roundup in takeoff
+- begin edge case and bug testing for all works developed in prep for live trial for estimators
