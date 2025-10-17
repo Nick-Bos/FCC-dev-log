cf37f980aa1395902d15d23babef40c9322b0a60
Nick Bos <roofingbos@gmail.com>
2025-10-17 17:12:46 +1030

    Add log file for 17-Oct-2025 to track updates and tasks for wastage factor tool

---
+++ planswift_logs/logs_17-Oct-2025_05-04PM.lua
@@ -0,0 +1,10 @@
+- Created wastage factor tool with HTML, CSS, and JavaScript
+- Hosted on netlify needs updating on netlify and link to github repo
+- Jobs for monday:
+- 1. Update netlify site with latest code from github repo 
+- 2. add additional content to webpage for wastage factor tool;
+-    a. handling logic for 'result' 
+-    b. possibly add logic for sheet count 
+- 3. continue bug testing
+- 4. refocus on 1350 vs 1200 selection issue and limitations with wall type selection and list population  
+
