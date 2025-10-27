932806cd114b1b54935a085d71670f0c946e4b4a
Nick Bos <roofingbos@gmail.com>
2025-10-27 16:06:34 +1030

    Add area template updates with bug fixes and new details tab

---
+++ planswift_logs/logs_27-Oct-2025_04-00PM.lua
@@ -0,0 +1,3 @@
+Fixed multiple bugs in area template today, added new features for better user experience.
+added details tab for job number, builder, supplier, address.
+need to test area template more thoroughly before finalizing.
