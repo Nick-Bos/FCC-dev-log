15464621afa5389218fd15d66803ef5d4d6e97c3
Nick Bos <roofingbos@gmail.com>
2025-10-27 16:06:34 +1030

    Add area template updates with bug fixes and new details tab

---
+++ planswift_formulas_functions/part-function.lua
@@ -0,0 +1,30 @@
+if '[Supplier]' = 'Knauf' then result := '[Wall Type 1200 (knauf)]'
+else 
+if '[Supplier]' = 'CSR' then result := '[Wall Type 1200 (csr)]'
+else
+if '[Supplier]' = 'ADX' then result := '[Wall Type 1200 (adx)]'
+else result := 'unknown supplier'
+
+    if '[Supplier]' = 'Knauf' then result := '[Wall Type 1350 (knauf)]'
+else 
+if '[Supplier]' = 'CSR' then result := '[Wall Type 1350 (csr)]'
+else
+if '[Supplier]' = 'ADX' then result := '[Wall Type 1350 (adx)]'
+else result := 'unknown supplier'
+
+    Wall Type 1350 (adx)
+    Wall Type 1350 (csr)
+    Wall Type 1350 (Knauf) 
+
+    if '[Supplier]' = 'Knauf' then result := '[Wall Type 1200 (knauf)]'
+else 
+if '[Supplier]' = 'CSR' then result := '[Wall Type 1200 (csr)]'
+else
+if '[Supplier]' = 'ADX' then result := '[Wall Type 1200 (adx)]'
+else 
+if '[Supplier]' = 'Knauf' then result := '[Wall Type 1350 (knauf)]'
+else 
+if '[Supplier]' = 'CSR' then result := '[Wall Type 1350 (csr)]'
+else
+if '[Supplier]' = 'ADX' then result := '[Wall Type 1350 (adx)]'
+else result := 'unknown supplier'
