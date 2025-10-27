86f9b8ffd8969c0e30c71e7ab54477627de98b10
Nick Bos <roofingbos@gmail.com>
2025-10-27 15:54:59 +1030

    Add conditional statements for wall types based on ceiling heights

---
+++ planswift_formulas_functions/wall-type-x-1350_input-conditional-statement.lua
@@ -0,0 +1,13 @@
+('[Wall Type]' = '13RE') and (('[Ceiling Height]' = 2.7) or (([Ceiling Height] > 3.2) and ([Ceiling Height] < 3.4)) or ('[Ceiling Height]' = 3.9) or ('[Ceiling Height]' = 4.5))
+('[Wall Type]' = '10SS') and (('[Ceiling Height]' = 2.7) or (([Ceiling Height] > 3.2) and ([Ceiling Height] < 3.4)) or ('[Ceiling Height]' = 3.9) or ('[Ceiling Height]' = 4.5))
+('[Wall Type]' = '10KSS') and (('[Ceiling Height]' = 2.7) or (([Ceiling Height] > 3.2) and ([Ceiling Height] < 3.4)) or ('[Ceiling Height]' = 3.9) or ('[Ceiling Height]' = 4.5))
+('[Wall Type]' = '13KSS') and (('[Ceiling Height]' = 2.7) or (([Ceiling Height] > 3.2) and ([Ceiling Height] < 3.4)) or ('[Ceiling Height]' = 3.9) or ('[Ceiling Height]' = 4.5))
+('[Wall Type]' = '10WR') and (('[Ceiling Height]' = 2.7) or (([Ceiling Height] > 3.2) and ([Ceiling Height] < 3.4)) or ('[Ceiling Height]' = 3.9) or ('[Ceiling Height]' = 4.5))
+('[Wall Type]' = '13WR') and (('[Ceiling Height]' = 2.7) or (([Ceiling Height] > 3.2) and ([Ceiling Height] < 3.4)) or ('[Ceiling Height]' = 3.9) or ('[Ceiling Height]' = 4.5))
+('[Wall Type]' = '13FC') and (('[Ceiling Height]' = 2.7) or (([Ceiling Height] > 3.2) and ([Ceiling Height] < 3.4)) or ('[Ceiling Height]' = 3.9) or ('[Ceiling Height]' = 4.5))
+('[Wall Type]' = '16FC') and (('[Ceiling Height]' = 2.7) or (([Ceiling Height] > 3.2) and ([Ceiling Height] < 3.4)) or ('[Ceiling Height]' = 3.9) or ('[Ceiling Height]' = 4.5))
+('[Wall Type]' = '6VILLA') and (('[Ceiling Height]' = 2.7) or (([Ceiling Height] > 3.2) and ([Ceiling Height] < 3.4)) or ('[Ceiling Height]' = 3.9) or ('[Ceiling Height]' = 4.5))
+('[Wall Type]' = '9VILLA') and (('[Ceiling Height]' = 2.7) or (([Ceiling Height] > 3.2) and ([Ceiling Height] < 3.4)) or ('[Ceiling Height]' = 3.9) or ('[Ceiling Height]' = 4.5))
+('[Wall Type]' = '13MRFC') and (('[Ceiling Height]' = 2.7) or (([Ceiling Height] > 3.2) and ([Ceiling Height] < 3.4)) or ('[Ceiling Height]' = 3.9) or ('[Ceiling Height]' = 4.5))
+// Condition for wall types based on ceiling heights
+// (2.7, 3.3, 3.9, or 4.5 meters). If both conditions are met, it returns true; otherwise, it returns false.
